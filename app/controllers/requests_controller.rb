ffc18d0e64f76f8f841852dadecab0caa021419f
Archie Gilbert <archiegilbert@me.com>
2017-04-25 11:00:57 +0100

    Referral system is in place with appropriate error messages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,15 +2,33 @@ class RequestsController < ApplicationController
 
   def reference
     @request = Request.new(request_params)
+
+    if !(@request.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i && @request.name.length > 2)
+      flash[:alert] = "Please make sure your email is correct and your name is more than 2 characters!"
+      redirect_to root_path
+    elsif Request.where(email: @request.email).count > 0
+      flash[:alert] = "#{@request.email} has already registered!"
+      redirect_to root_path
+    end
+
+    session[:request_info] = [@request.name, @request.email]
   end
 
   def create
-    @request = Request.create(request_params)
-    raise
-    if @request.save
+    @request = Request.new(request_params)
+    @request.name = session[:request_info][0]
+    @request.email = session[:request_info][1]
+
+    if Request.where(email: @request.reference).count > 0
+      flash[:alert] = "#{@request.reference} has already registered"
+      redirect_to root_path
+    elsif Request.where(reference: @request.reference).count > 0
+      flash[:alert] = "#{@request.reference} has already been referred"
+      redirect_to root_path
+    elsif @request.save
       # flash[:notice] = "Thank You! We'll be in touch!"
+      redirect_to root_path
     else
-      flash[:alert] = "Error! Please fill in al fields correctly!"
       redirect_to root_path
     end
 
