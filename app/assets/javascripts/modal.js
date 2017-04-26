408d7f3c2b882dad06dccb51d0ad450f0ddb0853
Archie Gilbert <archiegilbert@me.com>
2017-04-26 14:10:27 +0100

    modals moved to partials, style set except for that random god damn white line

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,15 +1,21 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var friend_modal = document.getElementById('myModal');
+var thanks_modal = document.getElementById('thanks_modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("request");
+var friend_button = document.getElementById("request");
+var thanks_button = document.getElementById("tick");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+friend_button.onclick = function() {
+  friend_modal.style.display = "block";
+}
+
+thanks_button.onclick = function() {
+  thanks_modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -18,8 +24,21 @@ btn.onclick = function() {
 // }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// $(window.modal).onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+$('#close-modal').click(function(){
+  friend_modal.style.display = "none";
+});
+
+$('#tick').click(function(){
+  friend_modal.style.display = "none";
+});
+
+$('#cancel').click(function(){
+  // thanks_modal.style.display = "none";
+  window.location.reload();
+});
